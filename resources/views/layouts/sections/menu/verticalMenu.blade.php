be586d4e784aab43cb2a31dbf346aac5800e1829
anas <anasben3issa@gmail.com>
2022-10-18 18:12:49 +0100

    fixed logo

--- resources/views/layouts/sections/menu/verticalMenu.blade.php
+++ resources/views/layouts/sections/menu/verticalMenu.blade.php
@@ -4,13 +4,8 @@
   <div class="app-brand demo">
     <a href="{{url('/')}}" class="app-brand-link">
       <span class="app-brand-logo demo">
-        @include('_partials.macros',["width"=>25,"withbg"=>'#696cff'])
+        @include('_partials.wiggler',["width"=>150,"height"=>222,"withbg"=>'#696cff'])
       </span>
-      <span class="app-brand-text demo menu-text fw-bold ms-2">{{config('variables.templateName')}}</span>
-    </a>
-
-    <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-autod-block d-xl-none">
-      <i class="bx bx-chevron-left bx-sm align-middle"></i>
     </a>
   </div>
 
