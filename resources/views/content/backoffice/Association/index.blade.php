43227a42596205208d81b1069f5a90d9426a8ace
Cyrine Louati <cyrine.louati@esprit.tn>
2022-10-19 01:39:29 +0100

    Fixed blade association/show

--- resources/views/content/backoffice/Association/index.blade.php
+++ resources/views/content/backoffice/Association/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts/contentNavbarLayout')
 
-@section('title', 'Tables - Basic Tables')
+@section('title', 'Wiggler - Associations')
 
 @section('content')
 <h4 class="fw-bold py-3 mb-4">
