590919aaea38fcbd5a80edabadf8ed7286d3448e
anas <anasben3issa@gmail.com>
2022-10-28 12:28:08 +0100

    improved display in reward frontoffice

--- resources/views/content/frontoffice/Reward/index.blade.php
+++ resources/views/content/frontoffice/Reward/index.blade.php
@@ -3,13 +3,11 @@
 
 <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Wiggler / HomePage </span> / Reward</h4>
 <h5 class="pb-1 mb-4">Rewards</h5>
-<div class="row mb-5">
+<div class="row row-cols-1 row-cols-md-3 g-4 mb-5">
   @foreach($rewards as $reward)
   <div class="col-md">
     <div class="card mb-3">
-      <div class="col-md-4">
-        <img class="card-img card-img-left" src="{{asset('assets/img/wiggler/reward.png')}}" alt="Card image" />
-      </div>
+      <img class="card-img card-img-left" src="{{asset('assets/img/wiggler/reward.png')}}" alt="Card image" />
       <div class="card-body">
         <h5 class="card-title">{{$reward->name}}</h5>
       </div>
